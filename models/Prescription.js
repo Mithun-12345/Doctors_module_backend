8d52de53457b06bccc2cfc0b28d15efc879d89ee
Abhishek-150604 <abhishek@maarrs.com>
2025-09-10 12:11:34 +0530

    Fixed Prescription Model enum issue

--- models/Prescription.js
+++ models/Prescription.js
@@ -117,7 +117,7 @@ const prescriptionItemSchema = new mongoose.Schema({
   },
   uom: {
     type: String,
-    enum: ["Gram", "Dram", "ML", "Pieces"],
+    enum: ["Graam", "Dram", "ML", "Pieces"],
     required: false,
   },
   dispenseQuantity: {
